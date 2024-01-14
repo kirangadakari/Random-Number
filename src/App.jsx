58f87551fbdc32cf265c24fcfb6bd800492ff474
kirangadakari <kirangadakari.2000@gmail.com>
2024-01-14 20:26:15 +0530

    Random commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {KgButton} from"./KgButton";
+import Hello from"./Hello";
+import Random from"./Random";
+function App(){
+  return<div>
+     <h1>Hello kiran THIS IS A BEST</h1>
+    <Hello/>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+     </div>
+  
+
+}
+export default App;
---
+++ src/Hello.jsx
@@ -0,0 +1,11 @@
+function Hello() {
+  let myName='Prashant';
+  let number=456;
+  let fullname=()=>{
+    return 'prashant jain'
+  }
+  return<h3>
+    Message No:{number} Hello this is a the future speaking{fullname()}
+  </h3>
+}
+export default Hello;
